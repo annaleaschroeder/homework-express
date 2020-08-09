493fe62a6b87981bcafc783e85c3bb1265fcc53a
janhenrikfock <jan.henrik.fock@googlemail.com>
2020-08-09 16:33:34 +0200

    Wriing into db.json

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import db from './db.json'
+import { v4 as uuidv4 } from 'uuid'
+import fs from 'fs'
 
 const server = express()
-const students = ['Anna', 'Caro', 'Deniz', 'Nina']
+//const students = ['Anna', 'Caro', 'Deniz', 'Nina']
 
 server.use(cors())
 server.use(express.json()) //parses incoming request with json payload
@@ -20,7 +23,10 @@ server.post('/', (req, res) => {
   console.log('Ich hab einen post request bekommen')
   console.log(req.body)
 
-  students.push(req.body.name)
+  db.students.push({ id: uuidv4(), name: req.body.name })
+  fs.writeFile('./db.json', JSON.stringify(db, null, 2), 'utf8', (error) =>
+    console.log(error)
+  )
   res.json({
     'response-message': 'Ich habe deinen request bekommen',
     request: req.body,
